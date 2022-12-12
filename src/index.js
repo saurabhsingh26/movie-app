95e3e9d0b328e9c04c4a0545257a6b8849929ecc
thesaurabhsingh <80765330+thesaurabhsingh@users.noreply.github.com>
2022-12-12 14:53:49 +0530

    Middleware

--- src/index.js
+++ src/index.js
@@ -1,11 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
 import './styles/index.css';
 import App from './components/App';
 import rootReducer from './reducers';
 
-const store = createStore(rootReducer); // pass the reducer which is movie
+// Curried form of function logger(obj,next,action)
+// logger(obj)(next)(action)
+const logger = function (obj){ // this object will contain 2 properties dispatch and getState properties. Redux willautomatically pass the two prop in logger function.
+  const {dispatch,getState} = obj  // {dispatch,getState} - this is not store object this is simple object with two parameters.
+  return function(next){
+    return function(action){
+      // Middleware
+      console.log('ACTION_TYPE',action.type);
+      next(action);
+    }
+  }
+}
+
+const store = createStore(rootReducer, applyMiddleware(logger)); // pass the reducer which is movie
 // createStore funx internally call the reducer to get initial state
 // dispatch function will pass the action to reducer
 // console.log('state',store.getState());
